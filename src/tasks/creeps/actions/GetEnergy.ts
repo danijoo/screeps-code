1d8ebbd0cda0a604af8b60656b600a69728405a4
danijoo <danijob88@googlemail.com>
2022-08-06 10:25:57 +0200

    getenergy from containers and storages

--- src/tasks/creeps/actions/GetEnergy.ts
+++ src/tasks/creeps/actions/GetEnergy.ts
@@ -1,6 +1,9 @@
 import {Task} from "../../../os/Task"
 import {TASK_CREEP_ACTION_GET_ENERGY} from "../../taskNames"
 
+type StorageStructure = Structure<STRUCTURE_CONTAINER> | Structure<STRUCTURE_STORAGE>
+type EnergySource = Resource<RESOURCE_ENERGY> | StorageStructure
+
 export class GetEnergy extends Task {
     readonly type: string = TASK_CREEP_ACTION_GET_ENERGY
 
@@ -11,28 +14,31 @@ export class GetEnergy extends Task {
             return true
         }
 
-        let resourceDrop: Resource | null = Game.getObjectById<Resource>(this.data.resourceId)
-        if (!resourceDrop) {
-            resourceDrop = this.findResource(creep) ?? null
-            if (resourceDrop)
-                this.data.resourceId = resourceDrop.id
+        let energySource: EnergySource | null = Game.getObjectById<EnergySource>(this.data.resourceId)
+        if (!energySource) {
+            energySource = this.findBestEnergySource(creep) ?? null
+            if (energySource)
+                this.data.resourceId = energySource.id
             else {
                 console.log("Failed to get energy: no resources found")
                 return true
             }
         }
 
-        if (!resourceDrop) {
+        if (!energySource) {
             console.log("Failed to get energy: no resources found")
             return true
         } else {
-            const result = creep.pickup(resourceDrop)
+            // @ts-ignore
+            const result = (energySource.structureType)?
+                // @ts-ignore
+                creep.withdraw(energySource, RESOURCE_ENERGY) : creep.pickup(energySource)
             switch (result) {
                 case OK:
                 case ERR_FULL:
                     return creep.store.getFreeCapacity() === 0
                 case ERR_NOT_IN_RANGE:
-                    creep.moveTo(resourceDrop, {visualizePathStyle: {}})
+                    creep.moveTo(energySource, {visualizePathStyle: {}})
                     return false
                 default:
                     console.log("Failed to get energy: " + result)
@@ -41,22 +47,39 @@ export class GetEnergy extends Task {
         }
     }
 
-    findResource(creep: Creep): Resource | undefined {
-        let resourceDrops = creep.room.find(FIND_DROPPED_RESOURCES,
+    // find the closest energy source that matches the requested amount
+    // if no source is big enough, return a smaller one
+    // tries to find the best as well as closest match
+    findBestEnergySource(creep: Creep): EnergySource | undefined {
+        const energyDrops: Resource<RESOURCE_ENERGY>[] = creep.room.find(FIND_DROPPED_RESOURCES,
             { filter: r => r.resourceType === RESOURCE_ENERGY })
-        if (resourceDrops.length > 1) {
-            let sizeSortedResources = resourceDrops.sort((a, b) => a.amount - b.amount)
+        const storageStructures: Structure[] = creep.room.find(FIND_STRUCTURES,
+            { filter: s => {
+                    return (s.structureType === STRUCTURE_CONTAINER || s.structureType === STRUCTURE_STORAGE)
+                    && s.store.getUsedCapacity(RESOURCE_ENERGY) > 0
+                }})
+        let energySources = (energyDrops as EnergySource[]).concat(storageStructures as EnergySource[])
+        // @ts-ignore
+
+
+        // if there are multiple energy sources, check if we can find one that is big enough
+        // otherwise, go on with all sources
+        const getAmount = (src: EnergySource) => (src.structureType)? src.store.getUsedCapacity(RESOURCE_ENERGY): src.amount
+        energySources = energySources.sort((a, b) => getAmount(a) - getAmount(b))
+        if (energySources.length > 1) {
             let requiredSize = creep.store.getFreeCapacity()
-            if (sizeSortedResources[0].amount < requiredSize) {
-                let largeResources = sizeSortedResources.filter(r => r.amount >= requiredSize)
-                if (largeResources.length > 0) {
-                    resourceDrops = largeResources
+            if (getAmount(energySources[0]) < requiredSize) {
+                let largestEnergySources = energySources.filter(src => getAmount(src) >= requiredSize)
+                if (largestEnergySources.length > 0) {
+                    energySources = largestEnergySources
                 }
             }
         }
-        resourceDrops = resourceDrops.sort((a, b) => a.pos.getRangeTo(creep.pos) - b.pos.getRangeTo(creep.pos))
-        if (resourceDrops.length > 0)
-            return resourceDrops[0]
+
+        // return the closest source
+        energySources = energySources.sort((a, b) => a.pos.getRangeTo(creep.pos) - b.pos.getRangeTo(creep.pos))
+        if (energySources.length > 0)
+            return energySources[0]
         return undefined
     }
 
--- test/unit/tasks/creeps/actions/GetEnergy.test.ts
+++ test/unit/tasks/creeps/actions/GetEnergy.test.ts
@@ -2,38 +2,53 @@ import {mockGlobal, mockInstanceOf} from "screeps-jest";
 import {Kernel} from "../../../../../src/os/Kernel";
 import {GetEnergy} from "../../../../../src/tasks/creeps/actions/GetEnergy"
 
-let mockResource: Resource<RESOURCE_ENERGY>
+let mockResources: Resource<RESOURCE_ENERGY>[]
+let mockContainers: StructureContainer[]
 let mockCreep: Creep
 let task: GetEnergy
 beforeEach(() => {
-    mockResource = mockInstanceOf<Resource<RESOURCE_ENERGY>>({
-        id: "resourceid",
+    mockResources = [mockInstanceOf<Resource<RESOURCE_ENERGY>>({
+        id: "resourceId",
         resourceType: RESOURCE_ENERGY,
         amount: 1000,
         pos: mockInstanceOf<RoomPosition>({
             getRangeTo: () => 5
-        })
+        }),
+        structureType: undefined,
+    })]
+    mockContainers = [mockInstanceOf<StructureContainer>({
+        id: "containerId",
+        store: {
+            getUsedCapacity: (resourceType: ResourceConstant) => (resourceType === RESOURCE_ENERGY) ? 1000 : 0
+        },
+        pos: mockInstanceOf<RoomPosition>({
+            getRangeTo: () => 7
+        }),
+        structureType: STRUCTURE_CONTAINER,
+    })]
+    const mockRoom = mockInstanceOf<Room>({
+        find: (type: number) => type === FIND_DROPPED_RESOURCES ? mockResources : mockContainers
     })
     mockCreep = mockInstanceOf<Creep>({
-        moveTo: () => OK,
-        pickup: () => OK,
+        pickup: jest.fn(),
+        moveTo: jest.fn(),
+        withdraw: jest.fn(),
         pos: new RoomPosition(5, 5, "roomName"),
         store: {
             getFreeCapacity: () => 0
         },
-        room: {
-            find: () => [mockResource]
-        }
+        room: mockRoom
     })
     mockGlobal<Game>("Game", {
         getObjectById: (id: string): any => {
-            if (id === "creepId") {
-                return mockCreep
-            }
-            return undefined
+            return {
+                "creepId": mockCreep,
+                "resourceId": mockResources[0],
+                "containerId": mockContainers[0]
+            }[id]
         },
         creeps: {
-            "creepId": mockCreep
+            creepId: mockCreep
         }
     })
     task = new GetEnergy(
@@ -45,7 +60,7 @@ beforeEach(() => {
     )
 })
 
-it("should stop if no resource pile is not found",() => {
+it("should stop if no resources are not found",() => {
     // @ts-ignore
     mockCreep.room.find = () => []
     task.run()
@@ -61,6 +76,7 @@ it("should stop if creep is not found",() => {
 })
 
 it("should stop if pickup was OK/store is full",() => {
+    mockCreep.pickup = jest.fn().mockReturnValueOnce(OK)
     task.run()
     // @ts-ignore
     expect(mockCreep.pickup).toHaveBeenCalledTimes(1)
@@ -80,7 +96,7 @@ it("should move to resource if more than one step away and not stop",() => {
     task.run()
     expect(mockCreep.pickup).toHaveBeenCalledTimes(1)
     expect(mockCreep.moveTo).toHaveBeenCalledTimes(1)
-    expect(mockCreep.moveTo).toHaveBeenCalledWith(mockResource, expect.anything())
+    expect(mockCreep.moveTo).toHaveBeenCalledWith(mockResources[0], expect.anything())
     expect(task.finished).not.toBeTruthy()
 })
 
@@ -107,11 +123,13 @@ it("should pickup from closest resource pile", () => {
         amount: 1000,
         pos: mockInstanceOf<RoomPosition>({
             getRangeTo: () => 3
-        })
+        }),
+        structureType: undefined
     })
-    mockCreep.room.find = () => {return [mockResource, closeResource]}
+    mockResources.push(closeResource)
     // @ts-ignore
     mockCreep.store.getFreeCapacity = () => {return 50}
+    mockCreep.pickup = jest.fn().mockReturnValue(OK)
     task.run()
     expect(mockCreep.pickup).toHaveBeenCalledTimes(1)
     expect(mockCreep.pickup).toHaveBeenCalledWith(closeResource)
@@ -119,20 +137,31 @@ it("should pickup from closest resource pile", () => {
 })
 
 it("should ignore small resource piles if there are bigger deposits", () => {
-    mockResource.amount = 5
+    mockContainers = []
+    mockResources[0].amount = 5
     const largeResource = mockInstanceOf<Resource<RESOURCE_ENERGY>>({
         id: "resourceIdLarge",
         resourceType: RESOURCE_ENERGY,
         amount: 70,
         pos: mockInstanceOf<RoomPosition>({
             getRangeTo: () => 20
-        })
+        }),
+        structureType: undefined
     })
-    mockCreep.room.find = () => {return [mockResource, largeResource]}
+    mockResources.push(largeResource)
+    mockCreep.pickup = jest.fn().mockReturnValue(OK)
     // @ts-ignore
     mockCreep.store.getFreeCapacity = () => {return 50}
     task.run()
     expect(mockCreep.pickup).toHaveBeenCalledTimes(1)
     expect(mockCreep.pickup).toHaveBeenCalledWith(largeResource)
     expect(task.finished).not.toBeTruthy()
+})
+
+it ("should withdraw from containers", () => {
+    mockResources = []
+    mockCreep.withdraw = jest.fn().mockReturnValue(OK)
+    task.run()
+    expect(mockCreep.withdraw).toHaveBeenCalledTimes(1)
+    expect(task.finished).toBeTruthy()
 })
